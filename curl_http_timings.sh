4c68c07884367c44d564eeeb9c3ab74b9c9e4034
Bernd Zeimetz <bernd@bzed.de>
2014-07-02 11:35:32 +0200

    Exit after 5 minutes, just in case.

--- curl_http_timings.sh
+++ curl_http_timings.sh
@@ -7,8 +7,9 @@ HOSTNAME="${COLLECTD_HOSTNAME:-$(hostname -f)}"
 INTERVAL="${COLLECTD_INTERVAL:-10}"
 
 
-
-while sleep "${INTERVAL}"; do
+# exit after ~5 minutes, just in case.... collectd will restart us.
+for i in `seq 1 30`; do
+        
 	TIME=$(date '+%s')
 	CURL_OUTPUT="PUTVAL ${HOSTNAME}/curl_timings-${NAME}/gauge-time_total  ${TIME}:%{time_total}\nPUTVAL ${HOSTNAME}/curl_timings-${NAME}/gauge-time_namelookup interval=${INTERVAL} ${TIME}:%{time_namelookup}\nPUTVAL ${HOSTNAME}/curl_timings-${NAME}/gauge-time_connect interval=${INTERVAL} ${TIME}:%{time_connect}\nPUTVAL ${HOSTNAME}/curl_timings-${NAME}/gauge-time_appconnect interval=${INTERVAL} ${TIME}:%{time_appconnect}\nPUTVAL ${HOSTNAME}/curl_timings-${NAME}/gauge-time_pretransfer interval=${INTERVAL} ${TIME}:%{time_pretransfer}\nPUTVAL ${HOSTNAME}/curl_timings-${NAME}/gauge-time_redirect interval=${INTERVAL} ${TIME}:%{time_redirect}\nPUTVAL ${HOSTNAME}/curl_timings-${NAME}/gauge-time_starttransfer interval=${INTERVAL} ${TIME}:%{time_starttransfer}\nPUTVAL ${HOSTNAME}/curl_timings-${NAME}/gauge-size_download interval=${INTERVAL} ${TIME}:%{size_download}\nPUTVAL ${HOSTNAME}/curl_timings-${NAME}/gauge-size_header interval=${INTERVAL} ${TIME}:%{size_header}\nPUTVAL ${HOSTNAME}/curl_timings-${NAME}/gauge-size_request interval=${INTERVAL} ${TIME}:%{size_request}\nPUTVAL ${HOSTNAME}/curl_timings-${NAME}/gauge-speed_download interval=${INTERVAL} ${TIME}:%{speed_download}\nPUTVAL ${HOSTNAME}/curl_timings-${NAME}/gauge-num_connects interval=${INTERVAL} ${TIME}:%{num_connects}\nPUTVAL ${HOSTNAME}/curl_timings-${NAME}/gauge-num_redirects interval=${INTERVAL} ${TIME}:%{num_redirects}\nPUTVAL ${HOSTNAME}/curl_timings-${NAME}/http_response_codes-http_code_%{http_code} interval=${INTERVAL} ${TIME}:1\n"
 
@@ -17,5 +18,6 @@ while sleep "${INTERVAL}"; do
         if [ $PPID -eq 1 ]; then
             exit 1
         fi
+        sleep "${INTERVAL}"
 done
 
